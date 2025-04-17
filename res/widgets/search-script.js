004234995cbd68c713f6f48fec4656fa1e944175
#BYDINOKI <danyfransds@gmail.com>
2025-04-16 20:49:28 -0600

    s

--- res/widgets/search-script.js
+++ res/widgets/search-script.js
@@ -203,7 +203,6 @@
       i.searchInput.addEventListener("keydown", function (event) {
 
         if (event.key === "Enter" || event.keyCode === 13) {
-          e.preventDefault(); 
           c();
           l(event.target.value);
         }
@@ -261,9 +260,7 @@ document.addEventListener("DOMContentLoaded", function() {
           for (let t = 0; t < r; t++) (e[t].query = n), s(f.compile(e[t]));
         })(d.search(t), t));
     }
-    function a(t) {
-      throw new Error("SimpleJekyllSearch --- " + t);
-    }
+    
     t.SimpleJekyllSearch = function (t) {
       var n;
       0 < o.validate(t).length &&
