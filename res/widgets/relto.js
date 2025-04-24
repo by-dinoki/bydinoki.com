08243171c9b978f1de7faeed39bf17ce98d52aa6
#BYDINOKI <danyfransds@gmail.com>
2025-04-23 22:33:30 -0600

    Update relto.js

--- res/widgets/relto.js
+++ res/widgets/relto.js
@@ -2,7 +2,7 @@
 
 texto = window.location.href ;
 const onrnd = "bydinoki.onrender.com";
-const gitt = "https://by-dinoki.github.io/bydinoki.com/";
+const gitt = "by-dinoki.github.io/bydinoki.com/";
 
 if (texto.includes(onrnd) || texto.includes(gitt)){
   
